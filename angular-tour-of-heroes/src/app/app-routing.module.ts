97113ae5b9dadae374eb7448b3dc8033a86f6cd5
Ryan <rybread@gmail.com>
2018-04-07 19:10:23 -0400

    beginning route

---
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+@NgModule({
+  exports:[ RouterModule ]
+})
+export class AppRoutingModule { }
